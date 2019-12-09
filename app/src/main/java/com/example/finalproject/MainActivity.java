648d58638ab142ea0884299086df9da72a8d4b47
John Hyde <jhyde2@zagmail.gonzaga.edu>
2019-12-09 14:59:41 -0800

    added cursor adapter

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,14 +4,22 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "MainActivityTag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,29 @@ public class MainActivity extends AppCompatActivity {
 
         ListingOpenHelper listingOpenHelper = new ListingOpenHelper(this);
         listingOpenHelper.insertListing(new Listing("address", "rent", 1, "1", "phone", "email", "length"));
+
+
+        ListView listView = findViewById(R.id.houseListView);
+
+        SimpleCursorAdapter cursorAdapter = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_list_item_2,
+                listingOpenHelper.getSelectAllListingsCursor(),
+                //parallel arrays... first is names of columns to get data FROM
+                new String[] {ListingOpenHelper.ADDRESS, ListingOpenHelper.BEDROOMS},
+                // ids of textviews to put data in
+                new int[] {android.R.id.text1, android.R.id.text2},
+                0
+        );
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+
+        listView.setAdapter(cursorAdapter);
 
     }
 
